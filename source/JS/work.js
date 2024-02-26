8e8e1a097de0a2dd7ab32d8d1c041bb41354c4bf
alkmanistik <erik.fattakhov.04@mail.ru>
2024-02-26 23:16:17 +0500

    JavaScript for change catalog

---
+++ source/JS/work.js
@@ -0,0 +1,40 @@
+var asepriteImage = document.getElementById('asepriteImage');
+var asepriteBlock = document.getElementById('asepriteBlock');
+var gameImage = document.getElementById('gameImage');
+var gameBlock = document.getElementById('gameBlock');
+var projectImage = document.getElementById('projectImage');
+var projectBlock = document.getElementById('projectBlock');
+
+const arrayImage = [asepriteImage, gameImage, projectImage];
+const arrayBlock = [asepriteBlock, gameBlock, projectBlock];
+
+asepriteImage.addEventListener('click', setAseprite);
+gameImage.addEventListener('click', setGame);
+projectImage.addEventListener('click', setGit);
+
+setItem(0);
+
+
+
+function setItem(index) {
+    arrayBlock.forEach((item) => {
+        item.style.display = 'none';
+      })
+    arrayImage.forEach((item) => {
+        item.style.backgroundColor = 'rgb(33, 37, 41)';
+      })
+    arrayBlock[index].style.display = 'block';
+    arrayImage[index].style.backgroundColor = 'black';
+}
+
+function setAseprite(){
+    setItem(0);
+}
+
+function setGame(){
+    setItem(1);
+}
+
+function setGit(){
+    setItem(2);
+}
