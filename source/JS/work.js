8545aa8eaca75c28ac632d5cb066dd7f9b2abd1e
alkmanistik <42465362+alkmanistik@users.noreply.github.com>
2025-02-12 21:58:42 +0500

    Dices app/ mobile works

--- source/JS/work.js
+++ source/JS/work.js
@@ -4,13 +4,16 @@ var gameImage = document.getElementById('gameImage');
 var gameBlock = document.getElementById('gameBlock');
 var projectImage = document.getElementById('projectImage');
 var projectBlock = document.getElementById('projectBlock');
+var mobileImage = document.getElementById('mobileImage');
+var mobileBlock = document.getElementById('mobileBlock');
 
-const arrayImage = [asepriteImage, gameImage, projectImage];
-const arrayBlock = [asepriteBlock, gameBlock, projectBlock];
+const arrayImage = [asepriteImage, gameImage, projectImage, mobileImage];
+const arrayBlock = [asepriteBlock, gameBlock, projectBlock, mobileBlock];
 
 asepriteImage.addEventListener('click', setAseprite);
 gameImage.addEventListener('click', setGame);
 projectImage.addEventListener('click', setGit);
+mobileImage.addEventListener('click', setMobile);
 
 setItem(0);
 
@@ -39,6 +42,10 @@ function setGit(){
     setItem(2);
 }
 
+function setMobile(){
+    setItem(3);
+}
+
 class CardData{
     constructor(
         title,
@@ -48,6 +55,10 @@ class CardData{
         youTubeLink = null,
         gameJamLink = null,
         gameLink = null,
+        appGallery = null,
+        ruStore = null,
+        googlePlay = null,
+        info = null,
     ){
         this.title = title;
         this.image = image;
@@ -56,6 +67,10 @@ class CardData{
         this.youTubeLink = youTubeLink;
         this.gameJamLink = gameJamLink;
         this.gameLink = gameLink;
+        this.appGallery = appGallery;
+        this.ruStore = ruStore;
+        this.googlePlay = googlePlay;
+        this.info = info;
     }
 }
 
@@ -108,29 +123,49 @@ var gitHubArray = [
         desciption = "Сайт о себе и мои работы",
         gitHubLink = "https://github.com/alkmanistik/alkmanistik.github.io",
     ),
+];
+var gitHubContainer = document.getElementById("gitHubContainer");
+
+var mobileArray = [
     new CardData(
         title = "GreenМир",
         image = "greenWorld.png",
         desciption = "Мобильное приложение вклада в экологию",
         gitHubLink = "https://github.com/alkmanistik/EnvironmentalProjectForHackaton",
     ),
+    // new CardData(
+    //     title = "Yard",
+    //     image = "yard.png",
+    //     desciption = "Мобильное приложение интернет магазин",
+    //     gitHubLink = "https://github.com/alkmanistik/coursework-2",
+    // ),
     new CardData(
-        title = "Yard",
-        image = "yard.png",
-        desciption = "Мобильное приложение интернет магазин",
-        gitHubLink = "https://github.com/alkmanistik/coursework-2",
+        title = "Кости",
+        image = "dices.png",
+        desciption = "Кидайте виртуальные кости",
+        gitHubLink = null,
+        youTubeLink = null,
+        gameJamLink = null,
+        gameLink = null,
+        appGallery = "https://appgallery.huawei.ru/app/C113453621",
+        ruStore = "https://www.rustore.ru/catalog/app/com.alkmanistik.diceapp",
+        googlePlay = null,
+        info = null,
     ),
 ];
-var gitHubContainer = document.getElementById("gitHubContainer");
+var mobileContainer = document.getElementById("mobileContainer")
 
 loadCard(asepriteContainer,asepriteArray, "source/images/asepriteWorks/");
 loadCard(gameContainer,gameArray, "source/images/gameWorks/");
 loadCard(gitHubContainer,gitHubArray, "source/images/gitHubWorks/");
+loadCard(mobileContainer, mobileArray, "source/images/mobileWorks/");
 
 function loadCard(container, array, sourceLink = "source/images/"){
-    array.forEach(element => {
+    array.forEach((element) => {
+        console.log("Processing element:", element);
+
         var desciptionString = "";
-        if (element.desciption!= null){
+        if (element.desciption != null){
             desciptionString = `<p class="card-text">${element.desciption}</p>`;
         }
         var gitHubString = "";
@@ -138,16 +173,28 @@ function loadCard(container, array, sourceLink = "source/images/"){
             gitHubString = `<a href="${element.gitHubLink}"><img src="source/images/logo/git.png" alt="gitHub logo"></a>`;
         }
         var youTubeString = "";
-        if (element.youTubeLink!= null){
+        if (element.youTubeLink != null){
             youTubeString = `<a href="${element.youTubeLink}"><img src="source/images/logo/youTube.png" alt="youTube logo"></a>`;
         }
         var gameJamString = "";
-        if (element.gameJamLink!= null){
+        if (element.gameJamLink != null){
             gameJamString = `<a href="${element.gameJamLink}"><img src="source/images/logo/gameJam.png" alt="gameJam logo"></a>`;
         }
         var gameString = "";
-        if (element.gameLink!= null){
+        if (element.gameLink != null){
             gameString = `<a href="${element.gameLink}"><img src="source/images/logo/gamepad.png" alt="game logo"></a>`;
+        }
+        var googlePlayString = "";
+        if (element.googlePlay!= null){
+            googlePlayString = `<a href="${element.googlePlay}"><img src="source/images/logo/googlePlay.png" alt="googlePlay logo"></a>`;
+        }
+        var appGalleryString = "";
+        if (element.appGallery!= null){
+            appGalleryString = `<a href="${element.appGallery}"><img src="source/images/logo/appGallery.png" alt="appGallery logo"></a>`;
+        }
+        var ruStoreString = "";
+        if (element.ruStore!= null){
+            ruStoreString = `<a href="${element.ruStore}"><img src="source/images/logo/ruStore.png" alt="ruStore logo"></a>`;
         }
         var optionString = `
             <p class="card-text">
@@ -155,6 +202,9 @@ function loadCard(container, array, sourceLink = "source/images/"){
                 ${gitHubString}
                 ${gameJamString}
                 ${gameString}
+                ${googlePlayString}
+                ${appGalleryString}
+                ${ruStoreString}
             </p>
             `
         var htmlString = `<div class="col-lg-6 col-md-6 col-sm-12 grid-column">
