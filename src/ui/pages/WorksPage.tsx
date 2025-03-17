fb6c25e62619c0f049543148311c1c888afcd6bb
alkmanistik <42465362+alkmanistik@users.noreply.github.com>
2025-03-17 10:11:51 +0500

    404 page and update works card

--- src/ui/pages/WorksPage.tsx
+++ src/ui/pages/WorksPage.tsx
@@ -33,15 +33,15 @@ export default function WorksPage() {
 
     return <div className="mt-24">
         <h1 className="mx-10 text-4xl font-bold">{workType}:</h1>
-        <div className="grid gap-8 mt-4 md:grid-cols-2 xl:grid-cols-3">
+        <div className="grid gap-6 my-6 md:grid-cols-2 xl:grid-cols-3">
             {workList.map((work, index) => (
                 <div key={index} className="flex justify-center">
-                    <div className="card bg-base-100 w-96 shadow-sm">
-                        <figure className="px-10 pt-10">
+                    <div className="card bg-base-200 h-fit w-68 sm:w-96 md:w-84 xl:w-96 shadow-md">
+                        <figure className="px-2 pt-2 sm:px-4 sm:pt-4 md:px-6 md:pt-6 xl:px-8 xl:pt-8">
                             <img
                                 src={work.imageSrc}
                                 alt={work.title}
-                                className="w-96 rounded-xl border-3 border-base-300"/>
+                                className="w-full rounded-xl border-3 border-base-300"/>
                         </figure>
                         <div className="card-body items-center text-center">
                             <h2 className="card-title">{work.title}</h2>
