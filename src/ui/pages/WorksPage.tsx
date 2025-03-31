bf00bc5c8e089e2b5273a37c15aad7b180d28e1a
alkmanistik <42465362+alkmanistik@users.noreply.github.com>
2025-03-31 12:15:14 +0500

    figma

--- src/ui/pages/WorksPage.tsx
+++ src/ui/pages/WorksPage.tsx
@@ -1,63 +1,84 @@
-import {useParams} from "react-router";
+import { useParams } from "react-router";
 import Work from "../../models/interfaces/Work.ts";
-import {asepriteWorks, gameWorks, gitHubWorks, mobileWorks} from "../../utils/constants.ts";
+import {
+    asepriteWorks,
+    figmaWorks,
+    gameWorks,
+    gitHubWorks,
+    mobileWorks,
+} from "../../utils/constants.ts";
 
 export default function WorksPage() {
-
-    const params = useParams<{ typeWork: string }>()
+    const params = useParams<{ typeWork: string }>();
 
     let workList: Array<Work> = [];
     let workType: string = "";
 
     switch (params.typeWork) {
         case "game":
-            workType = "Game"
-            workList = gameWorks
+            workType = "Game";
+            workList = gameWorks;
             break;
         case "github":
-            workType = "GitHub"
-            workList = gitHubWorks
+            workType = "GitHub";
+            workList = gitHubWorks;
             break;
         case "mobile":
-            workType = "Mobile"
-            workList = mobileWorks
+            workType = "Mobile";
+            workList = mobileWorks;
             break;
         case "aseprite":
-            workType = "Aseprite"
-            workList = asepriteWorks
+            workType = "Aseprite";
+            workList = asepriteWorks;
+            break;
+        case "figma":
+            workType = "Figma";
+            workList = figmaWorks;
             break;
         default:
-            workType = "Aseprite"
-            workList = asepriteWorks
+            workType = "Aseprite";
+            workList = asepriteWorks;
     }
 
-    return <div className="mt-24">
-        <h1 className="mx-10 text-4xl font-bold">{workType}:</h1>
-        <div className="grid gap-6 my-6 md:grid-cols-2 xl:grid-cols-3">
-            {workList.map((work, index) => (
-                <div key={index} className="flex justify-center">
-                    <div className="card bg-base-200 h-fit w-68 sm:w-96 md:w-84 xl:w-96 shadow-md">
-                        <figure className="px-2 pt-2 sm:px-4 sm:pt-4 md:px-6 md:pt-6 xl:px-8 xl:pt-8">
-                            <img
-                                src={work.imageSrc}
-                                alt={work.title}
-                                className="w-full rounded-xl border-3 border-base-300"/>
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{work.title}</h2>
-                            <p>{work.description}</p>
-                            <div className="card-actions">
-                                {work.linkList.map((link, index) => (
-                                        <a key={index} className="size-12 p-0" href={link.iconHref} target="_blank" rel="noopener noreferrer">
-                                            <img alt={link.iconName} src={link.iconSrc} className="rounded-xl"/>
+    return (
+        <div className="mt-24">
+            <h1 className="mx-10 text-4xl font-bold">{workType}:</h1>
+            <div className="grid gap-6 my-6 md:grid-cols-2 xl:grid-cols-3">
+                {workList.map((work, index) => (
+                    <div key={index} className="flex justify-center">
+                        <div className="card bg-base-200 h-fit w-68 sm:w-96 md:w-84 xl:w-96 shadow-md">
+                            <figure className="px-2 pt-2 sm:px-4 sm:pt-4 md:px-6 md:pt-6 xl:px-8 xl:pt-8">
+                                <img
+                                    src={work.imageSrc}
+                                    alt={work.title}
+                                    className="w-full rounded-xl border-3 border-base-300"
+                                />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{work.title}</h2>
+                                <p>{work.description}</p>
+                                <div className="card-actions">
+                                    {work.linkList.map((link, index) => (
+                                        <a
+                                            key={index}
+                                            className="size-12 p-0"
+                                            href={link.iconHref}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <img
+                                                alt={link.iconName}
+                                                src={link.iconSrc}
+                                                className="rounded-xl"
+                                            />
                                         </a>
-                                ))}
+                                    ))}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
-    </div>;
-
+    );
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,39 +2,46 @@ import WorkPath from "../models/interfaces/WorkPath.ts";
 import DescriptionAboutMe from "../models/interfaces/DescriptionAboutMe.ts";
 import Work from "../models/interfaces/Work.ts";
 
-import basicVibe from "../assets/profilePhoto/basicVibe.jpg"
-import cleanVibe from "../assets/profilePhoto/cleanVibe.jpg"
-import punkVibe from "../assets/profilePhoto/punkVibe.jpg"
-import summerVibe from "../assets/profilePhoto/summerVibe.jpg"
+import basicVibe from "../assets/profilePhoto/basicVibe.jpg";
+import cleanVibe from "../assets/profilePhoto/cleanVibe.jpg";
+import punkVibe from "../assets/profilePhoto/punkVibe.jpg";
+import summerVibe from "../assets/profilePhoto/summerVibe.jpg";
 
-import davidKing from "../assets/asepriteWorks/davidKing.jpg"
-import sam from "../assets/asepriteWorks/sam.png"
-import bt from "../assets/asepriteWorks/bt.png"
-import remor from "../assets/asepriteWorks/remor.jpg"
-import hoodwink from "../assets/asepriteWorks/hoodwink.gif"
+import davidKing from "../assets/asepriteWorks/davidKing.jpg";
+import sam from "../assets/asepriteWorks/sam.png";
+import bt from "../assets/asepriteWorks/bt.png";
+import remor from "../assets/asepriteWorks/remor.jpg";
+import hoodwink from "../assets/asepriteWorks/hoodwink.gif";
 
-import neverTrustCrabs from "../assets/gameWorks/neverTrustCrabs.jpg"
-import catsWisdom from "../assets/gameWorks/catsWisdom.png"
+import neverTrustCrabs from "../assets/gameWorks/neverTrustCrabs.jpg";
+import catsWisdom from "../assets/gameWorks/catsWisdom.png";
 
-import dices from "../assets/mobileWorks/dices.png"
-import greenWorld from "../assets/mobileWorks/greenWorld.png"
+import dices from "../assets/mobileWorks/dices.png";
+import greenWorld from "../assets/mobileWorks/greenWorld.png";
 
-import alkWeb from "../assets/gitHubWorks/alkWeb.png"
+import alkWeb from "../assets/gitHubWorks/alkWeb.png";
 
-import steam from "../assets/logo/steam.png"
-import email from "../assets/logo/email.png"
-import gitHub from "../assets/logo/gitHub.png"
-import telegram from "../assets/logo/telegram.png"
-import youtube from "../assets/logo/youtube.png"
-import gamepad from "../assets/logo/gamepad.png"
-import gameJam from "../assets/logo/gameJam.png"
-import appGallery from "../assets/logo/appGallery.png"
-import ruStore from "../assets/logo/ruStore.png"
+import watchShop from "../assets/figmaWorks/watchShop.png";
+
+import steam from "../assets/logo/steam.png";
+import email from "../assets/logo/email.png";
+import gitHub from "../assets/logo/gitHub.png";
+import telegram from "../assets/logo/telegram.png";
+import youtube from "../assets/logo/youtube.png";
+import gamepad from "../assets/logo/gamepad.png";
+import gameJam from "../assets/logo/gameJam.png";
+import appGallery from "../assets/logo/appGallery.png";
+import ruStore from "../assets/logo/ruStore.png";
+import figma from "../assets/logo/figma.png";
 
 export const workPaths: Array<WorkPath> = [
     {
         workName: "Aseprite",
         workHref: "/works/aseprite",
+    },
+    {
+        workName: "Figma",
+        workHref: "/works/figma",
     },
     {
         workName: "",
@@ -52,15 +59,21 @@ export const workPaths: Array<WorkPath> = [
         workName: "Mobile App",
         workHref: "/works/mobile",
     },
-]
+];
 
-export const profileImages: Array<string> = [summerVibe, cleanVibe, basicVibe, punkVibe]
+export const profileImages: Array<string> = [
+    summerVibe,
+    cleanVibe,
+    basicVibe,
+    punkVibe,
+];
 
 export const descriptionAboutMe: DescriptionAboutMe = {
     realName: "Эрик Фаттахов",
     nickname: "Alkmanistik",
-    bornYear: new Date(2004, 7, 7),  // year, month-1, day
-    description: "\n" +
+    bornYear: new Date(2004, 7, 7), // year, month-1, day
+    description:
+        "\n" +
         "Привет всем! На моём сайте выложены все мои работы. Я обычный программист, который занимается всем подряд. Стараюсь выкладывать работы с описанием, чтобы их можно было повторить. Использую современный стек технологий. P.S. Также люблю поиграть в игры с весёлой компанией.",
     contactList: [
         {
@@ -83,8 +96,8 @@ export const descriptionAboutMe: DescriptionAboutMe = {
             iconHref: "https://github.com/alkmanistik",
             iconSrc: gitHub,
         },
-    ]
-}
+    ],
+};
 
 export const asepriteWorks: Array<Work> = [
     {
@@ -117,7 +130,7 @@ export const asepriteWorks: Array<Work> = [
         description: "",
         linkList: [],
     },
-]
+];
 
 export const gameWorks: Array<Work> = [
     {
@@ -159,7 +172,7 @@ export const gameWorks: Array<Work> = [
             },
         ],
     },
-]
+];
 
 export const gitHubWorks: Array<Work> = [
     {
@@ -169,12 +182,13 @@ export const gitHubWorks: Array<Work> = [
         linkList: [
             {
                 iconName: "GitHub",
-                iconHref: "https://github.com/alkmanistik/alkmanistik.github.io",
+                iconHref:
+                    "https://github.com/alkmanistik/alkmanistik.github.io",
                 iconSrc: gitHub,
             },
         ],
     },
-]
+];
 
 export const mobileWorks: Array<Work> = [
     {
@@ -184,7 +198,8 @@ export const mobileWorks: Array<Work> = [
         linkList: [
             {
                 iconName: "GitHub",
-                iconHref: "https://github.com/alkmanistik/EnvironmentalProjectForHackaton",
+                iconHref:
+                    "https://github.com/alkmanistik/EnvironmentalProjectForHackaton",
                 iconSrc: gitHub,
             },
         ],
@@ -201,9 +216,26 @@ export const mobileWorks: Array<Work> = [
             },
             {
                 iconName: "RuStore",
-                iconHref: "https://www.rustore.ru/catalog/app/com.alkmanistik.diceapp",
+                iconHref:
+                    "https://www.rustore.ru/catalog/app/com.alkmanistik.diceapp",
                 iconSrc: ruStore,
             },
         ],
     },
-]
+];
+
+export const figmaWorks: Array<Work> = [
+    {
+        title: "Watch Shop App",
+        imageSrc: watchShop,
+        description: "Дизайн приложения для продажи часов",
+        linkList: [
+            {
+                iconName: "Figma",
+                iconHref:
+                    "https://www.figma.com/design/cQ8n8vA7osRA4s5BZG2d8J/Watch-Mobile-App?node-id=1-2&t=W9cdUjPMR9zCTEJu-1",
+                iconSrc: figma,
+            },
+        ],
+    },
+];
